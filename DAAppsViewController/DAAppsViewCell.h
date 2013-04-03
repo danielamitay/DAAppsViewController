1b74381e875d1086e238f11dd8a71e45d45cfad7
danielamitay <hello@danielamitay.com>
2013-04-03 18:42:43 -0400

    Add UITableViewCell subclass

---
+++ DAAppsViewController/DAAppsViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DAAppsViewCell.h
+//  DAAppsViewController
+//
+//  Created by Daniel Amitay on 4/3/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAAppsViewCell : UITableViewCell
+
+@end
---
+++ DAAppsViewController/DAAppsViewCell.m
@@ -0,0 +1,29 @@
+//
+//  DAAppsViewCell.m
+//  DAAppsViewController
+//
+//  Created by Daniel Amitay on 4/3/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import "DAAppsViewCell.h"
+
+@implementation DAAppsViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
