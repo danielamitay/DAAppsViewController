657668de944b71650daa2e740d41917d9a80bc74
danielamitay <hello@danielamitay.com>
2013-04-03 18:44:24 -0400

    Remove unnecessary method & comment

--- DAAppsViewController/DAAppsViewCell.m
+++ DAAppsViewController/DAAppsViewCell.m
@@ -13,17 +13,11 @@
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
+    if (self)
+    {
+        
     }
     return self;
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
