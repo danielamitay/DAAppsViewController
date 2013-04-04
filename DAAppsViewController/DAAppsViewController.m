07f69af20c4e9489fc72f3d2a1f7ff3bd04e821e
danielamitay <hello@danielamitay.com>
2013-04-04 17:34:19 -0400

    Add property methods and connection delegate methods

--- DAAppsViewController/DAAppsViewController.m
+++ DAAppsViewController/DAAppsViewController.m
@@ -18,6 +18,35 @@
 
 @implementation DAAppsViewController
 
+#pragma mark - Property methods
+
+- (void)setArtistId:(NSInteger)artistId
+{
+    _artistId = artistId;
+    if (_urlConnection)
+    {
+        [_urlConnection cancel];
+        _responseData = nil;
+    }
+    
+    NSString *countryCode = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+    NSMutableString *mutableRequestString = [[NSMutableString alloc] init];
+    [mutableRequestString appendFormat:@"http://itunes.apple.com/%@/", countryCode];
+    [mutableRequestString appendFormat:@"artist/id%i?dataOnly=true", _artistId];
+    
+    NSURL *requestURL = [[NSURL alloc] initWithString:mutableRequestString];
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] init];
+    [urlRequest setURL:requestURL];
+    [urlRequest setTimeoutInterval:30.0f];
+    [urlRequest setCachePolicy:NSURLRequestReloadRevalidatingCacheData];
+    [urlRequest setValue:@"iTunes-iPad/6.0 (6; 16GB; dt:73)" forHTTPHeaderField:@"User-Agent"];
+    _urlConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
+    
+    self.title = NSLocalizedString(@"Loading...",);
+}
+
+#pragma mark - View methods
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -43,6 +72,28 @@
     // Configure the cell...
     
     return cell;
+}
+
+#pragma mark - URL connection delegates
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    _responseData = [NSMutableData data];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+	[_responseData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    self.title = NSLocalizedString(@"Error",);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    [self.tableView reloadData];
 }
 
 @end
